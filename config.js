447ae8d36a480d67733f03bd919be0af7b7e1286
anchorpointbd <query.anchorpoint@gmail.com>
2026-08-21 20:35:00 +0600

    config: connect Supabase using public browser credentials

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.AP_CONFIG={SUPABASE_URL:"",SUPABASE_ANON_KEY:""};
+window.AP_CONFIG={SUPABASE_URL:"https://myyiyqerxfvwksejjqwx.supabase.co",SUPABASE_ANON_KEY:"sb_publishable_n7ITRK8bwPfy16-b0Rmz3g_iHxd0-Pl"};
